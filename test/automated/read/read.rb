97e4eb959b82ca047f44ce791b6196b3df0a71cc
Scott Bellware <sbellware@gmail.com>
2018-04-03 01:36:19 -0500

    Obsolete tests are removed

---
+++ test/automated/read/read.rb
@@ -0,0 +1,25 @@
+require_relative '../automated_init'
+
+context "Read" do
+  context "Transform Namespace" do
+    text = Controls::Text.example
+
+    [Controls::Subject::Transform, Controls::Subject::Transformer].each do |control|
+
+      context "#{control.name}" do
+        control_instance = control.example
+        example_class = control_instance.class
+
+        test "Converts text into an instance" do
+          instance = Read.(text, example_class, :some_format)
+          assert(instance == control_instance)
+        end
+
+        test "Class and format arguments can be transposed" do
+          instance = Read.(text, :some_format, example_class)
+          assert(instance == control_instance)
+        end
+      end
+    end
+  end
+end
