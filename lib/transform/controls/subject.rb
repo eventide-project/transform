9369f76b7e1dd0d2d56d24ce7de284f2991a057c
Scott Bellware <sbellware@gmail.com>
2018-04-03 01:48:41 -0500

    Control dependency is inverted so that Subject is an alias

--- lib/transform/controls/subject.rb
+++ lib/transform/controls/subject.rb
@@ -1,49 +1,13 @@
 module Transform
   module Controls
     module Subject
-      def self.example
-        instance = example_class.new
-        instance.some_attribute = Controls::RawData.example
-        instance
-      end
-
-      def self.example_class
-        Example
-      end
-
-      class Example
-        attr_accessor :some_attribute
-
-        def ==(other)
-          other.some_attribute == self.some_attribute
-        end
-
-        module Transformer
-          def self.some_format
-            SomeFormat
-          end
-
-          def self.instance(raw_data)
-            instance = Example.new
-            instance.some_attribute = raw_data
-            instance
-          end
-
-          def self.raw_data(instance)
-            instance.some_attribute
-          end
-
-          module SomeFormat
-            def self.write(raw_data)
-              Controls::Text.example
-            end
-
-            def self.read(text)
-              Controls::RawData.example
-            end
-          end
-        end
-      end
+      Transformer = Subject
     end
   end
 end
+module Transform
+  module Controls
+    Subject = Transform::Controls::Subject::Transformer
+  end
+end
+
